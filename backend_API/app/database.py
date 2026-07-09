7b66c8e72ac1c07d57f8388bef17b89c31ebafd0
Aashi Lad <aashilad22@gmail.com>
2026-07-09 21:42:22 +0530

    Update database.py
    Refactor database connection setup and add get_db function for session management.

--- backend_API/app/database.py
+++ backend_API/app/database.py
@@ -3,10 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = ""
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -15,3 +12,9 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
