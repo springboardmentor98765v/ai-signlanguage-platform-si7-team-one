03a5fb120cf3297a41559c2003912f82f7862063
Aashi Lad <aashilad22@gmail.com>
2026-08-16 13:10:03 +0530

    Add localhost to CORS allowed origins

--- backend_API/app/main.py
+++ backend_API/app/main.py
@@ -31,6 +31,7 @@ app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
