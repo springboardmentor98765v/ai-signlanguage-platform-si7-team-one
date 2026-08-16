fd9208a878e4e25ed01da0787d7ccafe9d6f455e
DivyaLathaSanaboyina <sdlatha9@gmail.com>
2026-08-16 14:54:05 +0530

    Add main.py businesslogic

--- Bussiness_Logic/main.py
+++ Bussiness_Logic/main.py
@@ -22,11 +22,13 @@ app = FastAPI(title="Practice Service (dev)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
